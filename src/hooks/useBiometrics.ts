9c6f2861d52c09668c26cc9bbdce2c904791e8e6
Nithesh <45152787+nk711@users.noreply.github.com>
2022-04-10 23:59:24 +0100

    Updated UseBiometric hook

--- src/hooks/useBiometrics.ts
+++ src/hooks/useBiometrics.ts
@@ -16,3 +16,67 @@ type LocalAuthenticationOptions = {
     fallbackLabel?: string;
 }
 
+const useBiometrics = () => {
+    const [isBiometricSupported, setIsBiometricSupported] = useState<Boolean>(false);
+    const [isUserEnrolled, setIsUserEnrolled] = useState<boolean>(false);
+    const [isBiometricAuthenticated, setIsBiometricAuthenticated]= useState<>(
+        {},
+    );
+    const [loading, setLoading] = useState<boolean>();
+    
+    //Check for Biometrics Support Permissions
+
+    const requestBiometricsPermissions = async():
+    Promise<any> => {
+        const isCompatible = await hasHardwareAsync();
+        setIsBiometricSupported(isCompatible);
+        const enrolled = await isEnrolledAsync();
+        setIsUserEnrolled(enrolled);
+    }
+
+    const handleBiometricAuth = useCallback(async() => {
+        if (!isBiometricSupported){
+            console.log('Biometrics is not supported on this device')
+        };
+        if (!setIsUserEnrolled) {
+            console.log('User has no biometrics enrolled')
+        };
+    }, [isBiometricSupported, isUserEnrolled])
+
+
+    const authenticate = async () => {
+        if (loading) return;
+        setLoading(true)
+
+        try {
+            const results = await authenticateAsync({
+                promptMessage: 'Login with Passcode',
+                disableDeviceFallback: false,
+            });
+            if (results.success) {
+              setIsBiometricAuthenticated(results);
+            }
+          } catch (e) {
+            console.log(e);
+          }
+      
+          setLoading(false);
+        };
+      
+        useEffect(() => {
+          requestBiometricsPermissions();
+        }, []);
+      
+        useEffect(() => {
+          handleBiometricAuth();
+        }, []);
+      
+        return [
+          isBiometricSupported,
+          isUserEnrolled,
+          isBiometricAuthenticated,
+          authenticate,
+        ] as const;
+    };
+      
+export default useBiometrics;
