b715312dfb86c66a044c5ae72005b505090a7c4b
Nithesh <45152787+nk711@users.noreply.github.com>
2022-04-12 23:59:53 +0100

    Minor changes

--- src/hooks/useBiometrics.ts
+++ src/hooks/useBiometrics.ts
@@ -4,22 +4,14 @@ import {
     hasHardwareAsync,
     authenticateAsync,
 } from 'expo-local-authentication';
-
-type Biometrics = {
-    authType: string, 
-}
-
-type LocalAuthenticationOptions = {
-    promptMessage? : string;
-    canelLabel?: string;
-    disableDeviceFallback?: boolean;
-    fallbackLabel?: string;
-}
+import * as Keychain from 'react-native-keychain';
+import { send_to_server } from "../utils/Server";
+import { Alert } from 'react-native';
 
 const useBiometrics = () => {
     const [isBiometricSupported, setIsBiometricSupported] = useState<Boolean>(false);
     const [isUserEnrolled, setIsUserEnrolled] = useState<boolean>(false);
-    const [isBiometricAuthenticated, setIsBiometricAuthenticated]= useState<>(
+    const [isAuthenticated, setIsAuthenticated]= useState<any>(
         {},
     );
     const [loading, setLoading] = useState<boolean>();
@@ -29,18 +21,14 @@ const useBiometrics = () => {
     const requestBiometricsPermissions = async():
     Promise<any> => {
         const isCompatible = await hasHardwareAsync();
+        const isEnrolled = await isEnrolledAsync();
         setIsBiometricSupported(isCompatible);
-        const enrolled = await isEnrolledAsync();
-        setIsUserEnrolled(enrolled);
+        setIsUserEnrolled(isEnrolled);
     }
 
     const handleBiometricAuth = useCallback(async() => {
-        if (!isBiometricSupported){
-            console.log('Biometrics is not supported on this device')
-        };
-        if (!setIsUserEnrolled) {
-            console.log('User has no biometrics enrolled')
-        };
+        if (!isBiometricSupported) console.log('Biometrics is not supported on this device')
+        if (!setIsUserEnrolled) console.log('User has no biometrics enrolled')
     }, [isBiometricSupported, isUserEnrolled])
 
 
@@ -49,16 +37,39 @@ const useBiometrics = () => {
         setLoading(true)
 
         try {
-            const results = await authenticateAsync({
+            const result = await authenticateAsync({
                 promptMessage: 'Login with Passcode',
                 disableDeviceFallback: false,
             });
-            if (results.success) {
-              setIsBiometricAuthenticated(results);
+           
+            if (result.success) {
+              setIsAuthenticated(result)
+              const credentials = await Keychain.getGenericPassword()
+              if (credentials) {                    
+                  console.log('Loaded user', credentials.username)
+                  const accessToken = credentials.password as string
+                  const result = await send_to_server(accessToken)
+                  // successfull
+                  return result
+              } else {
+                  // No credentials
+                  Alert.alert('NOT AUTHORISED', 'NO CREDENTIALS')
+              }
+            } 
+            else if (result.error==='unknown') {
+              console.log('unknown error')
             }
-          } catch (e) {
-            console.log(e);
-          }
+            else if (
+              result.error === 'user_cancel' ||
+              result.error === 'system_cancel' ||
+              result.error === 'app_cancel'
+            ) {
+               console.log('appcode')  
+            }
+  
+        } catch (e) {
+            console.log('An error has occured', (e as Error).message)
+        }
       
           setLoading(false);
         };
@@ -74,7 +85,7 @@ const useBiometrics = () => {
         return [
           isBiometricSupported,
           isUserEnrolled,
-          isBiometricAuthenticated,
+          isAuthenticated,
           authenticate,
         ] as const;
     };
